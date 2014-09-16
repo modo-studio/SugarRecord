46d35afb103fe40b934eddc7132eac0e48157641
Pedro Piñera <pepibumur@gmail.com>
2014-09-16 19:51:48 +0200

    Added more REALM tests

--- SugarRecordDemo/SugarRecordTests/SugarRecordTests.swift
+++ SugarRecordDemo/SugarRecordTests/SugarRecordTests.swift
@@ -74,7 +74,6 @@ class SugarRecordREALMTests: QuickSpec {
         }
         
         describe("object creation", { () -> () in
-            
             it("should create the item in database", { () -> () in
                 var realmObject: RealmObject = RealmObject.create() as RealmObject
                 realmObject.name = "Realmy"
@@ -90,9 +89,41 @@ class SugarRecordREALMTests: QuickSpec {
         
         describe("object deletion", { () -> () in
             beforeEach({ () -> () in
-                
             })
             afterEach({ () -> () in
+            })
+            it("should delete a given object properly", { () -> () in
+                var realmObject: RealmObject = RealmObject.create() as RealmObject
+                realmObject.name = "Realmy"
+                realmObject.age = 22
+                realmObject.email = "test@mail.com"
+                realmObject.city = "TestCity"
+                realmObject.birthday = NSDate()
+                let saved: Bool = realmObject.save()
+                realmObject.delete()
+                expect(RealmObject.allObjects().count).to(equal(0))
+            })
+            
+            it("should delete all the objects in the database", { () -> () in
+                var realmObject: RealmObject = RealmObject.create() as RealmObject
+                realmObject.name = "Realmy"
+                realmObject.age = 22
+                realmObject.email = "test@mail.com"
+                realmObject.city = "TestCity"
+                realmObject.birthday = NSDate()
+                let saved: Bool = realmObject.save()
+                
+                var realmObject2: RealmObject = RealmObject.create() as RealmObject
+                realmObject2.name = "Realmy"
+                realmObject2.age = 22
+                realmObject2.email = "test@mail.com"
+                realmObject2.city = "TestCity"
+                realmObject2.birthday = NSDate()
+                let saved2: Bool = realmObject2.save()
+
+                realmObject2.delete()
+                realmObject.delete()
+                expect(RealmObject.allObjects().count).to(equal(0))
             })
         });
         
