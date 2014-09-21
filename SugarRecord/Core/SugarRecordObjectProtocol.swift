08d95edbf5e392b209e212122023346e9e903c39
pepibumur <pepibumur@gmail.com>
2014-09-21 16:38:27 +0200

    Documented the SugarRecordObjectProtocol

--- SugarRecord/Core/SugarRecordObjectProtocol.swift
+++ SugarRecord/Core/SugarRecordObjectProtocol.swift
@@ -47,34 +47,72 @@ public protocol SugarRecordObjectProtocol
     
     //MARK - Sorting
     
+    /**
+    *  Returns a SugarRecord finder passing a sorter key
+    */
     class func sorted(by sorterKey: String, ascending: Bool) -> SugarRecordFinder
+    
+    /**
+    *  Returns a SugarRecord finder pasing a sort descriptor
+    */
     class func sorted(by sortDescriptor: NSSortDescriptor) -> SugarRecordFinder
+    
+    /**
+    *  Returns a SugarRecord finder passing a sort descriptor
+    */
     class func sorted(by sortDescriptors: [NSSortDescriptor]) -> SugarRecordFinder
     
     
     //MARK - All
     
+    /**
+    *  Returns a SugarRecord finder to fetch all items
+    */
     class func all() -> SugarRecordFinder
     
     
     //MARK - Deletion
     
+    /**
+    *  Deletes the object
+    */
     func delete() -> Bool
     
     
     //MARK - Creation
     
+    /**
+    *  Creates the object in the main context
+    */
     class func create() -> AnyObject
+    
+    /**
+    *  Creates the object in the given context
+    */
     class func create(inContext context: SugarRecordContext) -> AnyObject
     
     
     //MARK - Saving
     
+    /**
+    *  Saves the object
+    */
     func save () -> Bool
+    
+    /**
+    *  Saves the object sync/async with a completion closure
+    */
     func save (asynchronously: Bool, completion: (error: NSError) -> ())
     
     //MARK - BeginEditing
     
+    /**
+    *  Notifies the context that the object is going to be edited
+    */
     func beginEditing()
+    
+    /**
+    *  Notifies the context that the object edition has finished
+    */
     func endEditing()
 }
