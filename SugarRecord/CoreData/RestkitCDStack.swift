9718b2d70a685a829944ac9e0e1319462b0803f5
Pedro Piñera <pepibumur@gmail.com>
2014-09-28 00:21:30 +0200

    Started the Restkit integration

---
+++ SugarRecord/CoreData/RestkitCDStack.swift
@@ -0,0 +1,21 @@
+//
+//  RestkitCDStack.swift
+//  SugarRecord
+//
+//  Created by Pedro Piñera Buendía on 28/09/14.
+//  Copyright (c) 2014 SugarRecord. All rights reserved.
+//
+
+import Foundation
+
+public class RestkitCDStack: DefaultCDStack
+{
+    override public func initialize() {
+        //let store: RKManagedObjectStore = RKManagedObjectStore(self.managedObjectModel)
+        //store.addSQLitePersistentStoreAtPath(databasePath!, fromSeedDatabasePath:nil, withConfiguration: nil, options: nil, error: &error)
+        //store.createManagedObjectContexts
+        //self.persistentStoreCoordinator = store.persistentStoreCoordinator
+        //self.rootSavingContext = store.persistentStoreManagedObjectContext()
+        //self.mainContext = store.mainQueueManagedObjectContext
+    }
+}
