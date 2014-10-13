91ac06e0f2062656d917b1122d4aa4fd529ecf0a
Pedro Piñera Buendía <pepibumur@gmail.com>
2014-10-13 10:20:13 +0200

    Started iCloud stack

---
+++ SugarRecord/CoreData/iCloudCDStack.swift
@@ -0,0 +1,15 @@
+//
+//  iCloudCDStack.swift
+//  SugarRecord
+//
+//  Created by Pedro Piñera Buendía on 12/10/14.
+//  Copyright (c) 2014 SugarRecord. All rights reserved.
+//
+
+import Foundation
+
+public class RestkitCDStack: DefaultCDStack
+{
+
+
+}
