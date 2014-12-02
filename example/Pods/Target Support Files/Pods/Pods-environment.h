90d5e41be8c4c2fcea153eb830d95642f3642a52
Pedro <pepibumur@gmail.com>
2014-12-02 22:11:36 +0100

    Updated pospec

---
+++ example/Pods/Target Support Files/Pods/Pods-environment.h
@@ -0,0 +1,14 @@
+
+// To check if a library is compiled with CocoaPods you
+// can use the `COCOAPODS` macro definition which is
+// defined in the xcconfigs so it is available in
+// headers also when they are imported in the client
+// project.
+
+
+// SugarRecord/CoreData
+#define COCOAPODS_POD_AVAILABLE_SugarRecord_CoreData
+#define COCOAPODS_VERSION_MAJOR_SugarRecord_CoreData 1
+#define COCOAPODS_VERSION_MINOR_SugarRecord_CoreData 0
+#define COCOAPODS_VERSION_PATCH_SugarRecord_CoreData 3
+
