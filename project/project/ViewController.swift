de8b5d570b2aacf88f1daf9a969740f81f28c415
Pedro <pepibumur@gmail.com>
2014-12-10 23:37:14 +0100

    Removed the iPhone simulator line

--- project/project/ViewController.swift
+++ project/project/ViewController.swift
@@ -9,7 +9,6 @@
 import UIKit
 
 class ViewController: UIViewController {
-
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -19,7 +18,5 @@ class ViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
 }
 
