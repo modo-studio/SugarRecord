19ae0351a519558a63bdebf60765bf8ebe8697bf
Pedro <pepibumur@gmail.com>
2014-12-12 01:54:46 +0100

    Added the results protocol and added the realm example

--- library/CoreData/Base/SugarRecordCDResults.swift
+++ library/CoreData/Base/SugarRecordCDResults.swift
@@ -8,7 +8,7 @@
 
 import Foundation
 
-class SugarRecordCDResults: SugarRecordResultsProtocol
+class SugarRecordCDResults
 {
-    
+
 }
--- library/Realm/SugarRecordRLMResults.swift
+++ library/Realm/SugarRecordRLMResults.swift
@@ -7,8 +7,73 @@
 //
 
 import Foundation
+import Realm
 
 class SugarRecordRLMResults: SugarRecordResultsProtocol
 {
-    //TODO
+    //MARK: - Attributes
+    typealias T = RLMObject
+    typealias R = SugarRecordRLMResults
+    private var realmResults: RLMResults
+    
+    //MARK: - Constructors
+    
+    init(realmResults: RLMResults)
+    {
+        self.realmResults = realmResults
+    }
+    
+    //MARK: - SugarRecordResultsProtocol
+    
+    var count:UInt {
+        get {
+            return realmResults.count
+        }
+    }
+    
+    func objectAtIndex(index: UInt) -> T!
+    {
+        return realmResults.objectAtIndex(index) as T
+    }
+    
+    func firstObject() -> T!
+    {
+        return realmResults.firstObject() as T
+    }
+    
+    func lastObject() -> T!
+    {
+        return realmResults.lastObject() as T
+    }
+    
+    func indexOfObject(object: T) -> UInt
+    {
+        return realmResults.indexOfObject(object)
+    }
+    
+    func indexOfObjectWithPredicate(predicate: NSPredicate!) -> UInt
+    {
+        return realmResults.indexOfObjectWithPredicate(predicate)
+    }
+    
+    func objectsWithPredicate(predicate: NSPredicate!) -> R!
+    {
+        return SugarRecordRLMResults(realmResults: realmResults.objectsWithPredicate(predicate))
+    }
+    
+    func sortedResultsUsingProperty(property: String!, ascending: Bool) -> R!
+    {
+        return SugarRecordRLMResults(realmResults: realmResults.sortedResultsUsingProperty(property, ascending: ascending))
+    }
+    
+    func sortedResultsUsingDescriptors(properties: [AnyObject]!) -> R!
+    {
+        return SugarRecordRLMResults(realmResults: realmResults.sortedResultsUsingDescriptors(properties))
+    }
+    
+    subscript (index: UInt) -> T! {
+        get {
+            return realmResults[index] as T
+        }
+    }
 }
