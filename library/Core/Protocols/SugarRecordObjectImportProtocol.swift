64f5a6f45153dfe882715f1d21210f42ad96d8dd
Pedro <pepibumur@gmail.com>
2014-12-16 21:45:36 +0100

    Created protocols

---
+++ library/Core/Protocols/SugarRecordObjectImportProtocol.swift
@@ -0,0 +1,71 @@
+protocol SugarRecordObjectImportProtocol
+{
+	class func mapAttributes(#remoteObject: JSON, localObject: SugarRecordObjectProtocol, mappingModel: MappingModel)
+	
+	class func import(#object: JSON, inContext context: SugarRecordContext, scheme: MappinScheme?) -> SugarRecordObjectProtocol
+	class func import(#array: JSON, inContext context: SugarRecordContext, scheme: MappingScheme?) -> [SugarRecordObjectProtocol]
+
+}
+
+protocol SugarRecordObjectMappingProtocol
+{
+	//class func mappingModel(scheme: MappingScheme?) -> MappingModel
+	var mappingModel: MappingModel
+}
+
+enum MappingScheme
+{
+	case scheme(name: String)
+}
+
+enum MappingAttributeType
+{
+	case simpleAttribute, identiferAttribute,
+	case relationshipAttribute(let prefetch: Bool, dest: SugarRecordObjectImportProtocol)
+}
+
+struct MappingAttribute
+{
+	let localKey: String?
+	let remoteKey: String?
+	let type: MappingAttributeType?
+	// let class: ??????
+}
+
+class MappingModel
+{
+	var attributes: [MappingAttribute]
+
+	func prefetchRelationships() -> ([MappingAttribute])
+	func simpleRelationships() -> ([MappingAttribute])
+	func attributes() -> ([MappingAttribute])
+	func relationships() -> ([MappingAttribute])
+
+	class func builder() {
+		return MappingModel()
+	}
+
+	func add(attribute: String) -> MapingModel
+	 {
+		attributes.append
+	}
+}
+
+// return MappingModel.builder().
+// add("name").
+// add("project", )
+
+
+// Cache : [RemoteKey: [String: Anyobject]]
+
+
+// Caching steps
+// 1) Import array
+// 2) Create cache
+// 	2.1) Find prefetch attributes
+//	2.2) Find common objects in the JSON file
+//	2.3) Fetch common objects
+// 3) Map attributes
+// 4) Map relationships (with cache)
+
+// Pass mappingModel in method
---
+++ library/CoreData/Base/NSManagedObject+Import.swift
@@ -0,0 +1,15 @@
+//
+//  NSManagedObject+Import.swift
+//  SugarRecord
+//
+//  Created by Pedro Piñera Buendia on 07/09/14.
+//  Copyright (c) 2014 SugarRecord. All rights reserved.
+//
+
+import Foundation
+import CoreData
+
+extension NSManagedObject: SugarRecordObjectImportProtocol
+{
+
+}
