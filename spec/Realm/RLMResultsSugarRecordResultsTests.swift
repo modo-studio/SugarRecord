27654837de2fd5868bf696129ff4be0537b45a0b
Pedro <pepibumur@gmail.com>
2014-12-29 01:06:16 +0100

    Added some tests

---
+++ spec/Realm/RLMResultsSugarRecordResultsTests.swift
@@ -0,0 +1,19 @@
+//
+//  RLMResultsSugarRecordResultsTests.swift
+//  project
+//
+//  Created by Pedro Piñera Buendía on 29/12/14.
+//  Copyright (c) 2014 SugarRecord. All rights reserved.
+//
+
+import Foundation
+import Realm
+import XCTest
+
+class RLMResultsSugarRecordResultsTests: XCTestCase
+{
+    /**
+    *  Note: Cannot subclass RLMResults and modify the init() method to mock that class
+        - Tests are applied at the model level
+    */
+}
