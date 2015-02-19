aef4a08bfc9d4f9d6c7a52c6c6df5cab73dc47b3
David Chavez <david@psychosity.net>
2015-02-19 15:35:33 -0600

    changed @objc to the dynamic keyword

--- library/CoreData/Base/iCloudCDStack.swift
+++ library/CoreData/Base/iCloudCDStack.swift
@@ -335,7 +335,7 @@ public class iCloudCDStack: DefaultCDStack
     
     :param: notification Notification with these changes
     */
-    @objc internal func persistentStoreDidImportUbiquitousContentChanges(notification: NSNotification)
+    dynamic internal func persistentStoreDidImportUbiquitousContentChanges(notification: NSNotification)
     {
         SugarRecordLogger.logLevelVerbose.log("Changes detected from iCloud. Merging them into the current CoreData stack")
         self.rootSavingContext!.performBlock { [weak self] () -> Void in
@@ -355,7 +355,7 @@ public class iCloudCDStack: DefaultCDStack
     
     :param: notification Notification with these changes
     */
-    @objc internal func storesWillChange(notification: NSNotification)
+    dynamic internal func storesWillChange(notification: NSNotification)
     {
         SugarRecordLogger.logLevelVerbose.log("Stores will change, saving pending changes before changing store")
         self.saveChanges()
@@ -367,7 +367,7 @@ public class iCloudCDStack: DefaultCDStack
     
     :param: notification Notification with the information
     */
-    @objc internal func storeDidChange(notification: NSNotification)
+    dynamic internal func storeDidChange(notification: NSNotification)
     {
         SugarRecordLogger.logLevelVerbose.log("The persistent store of the psc did change")
         // Nothing to do here
