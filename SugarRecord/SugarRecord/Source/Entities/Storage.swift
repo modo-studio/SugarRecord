326e25bd87de50c20075eb24f5ce285b8047f9bc
Pedro Piñera <pepibumur@gmail.com>
2015-11-06 21:24:58 +0100

    Defined Storage protocol and started working on the CoreDataDefaultStack

--- SugarRecord/SugarRecord/Source/Entities/Storage.swift
+++ SugarRecord/SugarRecord/Source/Entities/Storage.swift
@@ -1,11 +1,47 @@
 import Foundation
 
-typealias Saver = (context: Context, save: () -> Void) -> Void
-
 /**
  *  Protocol that identifies a persistence storage
  */
-protocol Storage {
+public protocol Storage {
     
+    /// Main context. This context is mostly used for querying operations
     var mainContext: Context { get }
+    
+    /// Save context. This context is mostly used for save operations
+    var saveContext: Context { get }
+    
+    /// Memory context. This context is mostly used for testing (not persisted)
+    var memoryContext: Context { get }
+    
+    /**
+     Executes the provided operation in background
+     
+     - parameter operation: operation to be executed
+     - parameter save:      save closure that must be called to persist the changes
+     */
+    func operation(operation: (context: Context, save: () -> Void) -> Void)
+    
+    /**
+     Executes the provided operation in a given queue
+     Note: This method must be implemented by the Storage that conforms this protocol.
+     Some storages require propagating these saves across the stack of contexts (e.g. CoreData)
+     
+     - parameter queue:     queue where the operation will be executed
+     - parameter operation: operation to be executed
+     - parameter save:      save closure that must be called to persist the changes
+     */
+    func operation(queue: dispatch_queue_t, operation: (context: Context, save: () -> Void) -> Void)
+}
+
+
+// MARK: - Storage extension
+
+public extension Storage {
+    
+    func operation(operation: (context: Context, save: () -> Void) -> Void) {
+        let queue = dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_BACKGROUND, 0)
+        self.operation(queue, operation: operation)
+    }
+    
 }
