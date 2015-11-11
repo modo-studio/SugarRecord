5cff8ee01aeb90f9b8fef10546ce9a0e5c2a8fc5
Pedro Piñera <pepibumur@gmail.com>
2015-11-11 18:54:39 +0100

    Documented NSSortDescriptorConvertible

--- SugarRecord/SugarRecord/Source/Foundation/Protocols/NSSortDescriptorConvertible.swift
+++ SugarRecord/SugarRecord/Source/Foundation/Protocols/NSSortDescriptorConvertible.swift
@@ -1,6 +1,19 @@
 import Foundation
 
-protocol NSSortDescriptorConvertible {
+/**
+ *  Protocol that defines that the entity that conforms it contains a sort descriptor
+ */
+public protocol NSSortDescriptorConvertible {
+    
+    /**
+     Initializes the entity with a sort descriptor
+     
+     - parameter sortDescriptor: sort descriptor
+     
+     - returns: initialized NSSortDescriptorConvertible
+     */
     init(sortDescriptor: NSSortDescriptor)
+    
+    /// Sort descriptor
     var sortDescriptor: NSSortDescriptor? { get }
 }
