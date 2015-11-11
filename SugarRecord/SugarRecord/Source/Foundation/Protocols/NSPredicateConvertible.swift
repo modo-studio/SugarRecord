e39093991937faffcbc4f5899567e671781b5fa9
Pedro Piñera <pepibumur@gmail.com>
2015-11-11 18:54:10 +0100

    Documented NSPredicateConvertible

--- SugarRecord/SugarRecord/Source/Foundation/Protocols/NSPredicateConvertible.swift
+++ SugarRecord/SugarRecord/Source/Foundation/Protocols/NSPredicateConvertible.swift
@@ -1,6 +1,19 @@
 import Foundation
 
-protocol NSPredicateConvertible {
+/**
+ *  Protocol that defines that the entity that conforms it contains a predicate
+ */
+public protocol NSPredicateConvertible {
+    
+    /**
+     Initializes the entity with a predicate
+     
+     - parameter predicate: predicate
+     
+     - returns: initialized NSPredicateConvertible entity
+     */
     init(predicate: NSPredicate)
+    
+    /// Predicate
     var predicate: NSPredicate? { get }
 }
