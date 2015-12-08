93ecc42022a3f1fdd4bcb56d17a8b398015ec3d3
Pedro Piñera <pepibumur@gmail.com>
2015-12-08 17:34:57 +0100

    Added CoreData.Options tests

---
+++ SugarRecordTests/Source/CoreData/Entities/OptionsTests.swift
@@ -0,0 +1,35 @@
+import Foundation
+import Quick
+import Nimble
+import CoreData
+
+@testable import SugarRecord
+
+class OptionsTests: QuickSpec {
+    
+    override func spec() {
+        
+        describe("options dictionary content") {
+            
+            it("should return the right data for default options") {
+                let options = CoreData.Options.Default.dict()
+                let sqliteOptions = options[NSSQLitePragmasOption] as! [String: String]
+                expect(sqliteOptions["WAL"]) == "journal_mode"
+                expect(options[NSMigratePersistentStoresAutomaticallyOption] as? NSNumber) == NSNumber(bool: true)
+                expect(options[NSInferMappingModelAutomaticallyOption] as? NSNumber) == NSNumber(bool: false)
+            }
+            
+            it("should return the right data for migration options") {
+                let options = CoreData.Options.Migration.dict()
+                let sqliteOptions = options[NSSQLitePragmasOption] as! [String: String]
+                expect(sqliteOptions["WAL"]) == "journal_mode"
+                expect(options[NSMigratePersistentStoresAutomaticallyOption] as? NSNumber) == NSNumber(bool: true)
+                expect(options[NSInferMappingModelAutomaticallyOption] as? NSNumber) == NSNumber(bool: true)
+            }
+            
+        }
+        
+    }
+    
+}
+
