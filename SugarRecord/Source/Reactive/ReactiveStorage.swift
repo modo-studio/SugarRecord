86c14f8ee5bf1820b84c04ffa7eb9791bbcde403
Pepi <pepibumur@gmail.com>
2015-12-20 18:11:57 +0100

    Implemented storage reactive methods

--- SugarRecord/Source/Reactive/ReactiveStorage.swift
+++ SugarRecord/Source/Reactive/ReactiveStorage.swift
@@ -1,5 +1,6 @@
 import Foundation
 import ReactiveCocoa
+import RxSwift
 
 /**
  *  Protol that defines the Reactive interface of an Storage
@@ -17,7 +18,16 @@ public protocol ReactiveStorage {
     
     - returns: SignalProducer that executes the action.
     */
-    func rac_operation(operation: (context: Context, save: Saver) -> Void) -> SignalProducer<Void, NoError>
+    func rac_operation(op: (context: Context, save: Saver) -> Void) -> SignalProducer<Void, NoError>
+    
+    /**
+     Executes the given operation.
+     
+     - parameter operation: Operation to be executed. Context must be used to save your changes in and the save() closure must be called in order to get the changes persisted in the storage.
+     
+     - returns: Observable that executes the action.
+     */
+    func rx_operation(op: (context: Context, save: Saver) -> Void) -> Observable<Void>
     
     /**
      Executes the given operation in a background thread.
@@ -26,7 +36,16 @@ public protocol ReactiveStorage {
      
      - returns: SignalProducer that executes the action.
      */
-    func rac_backgroundOperation(operation: (context: Context, save: Saver) -> Void) -> SignalProducer<Void, NoError>
+    func rac_backgroundOperation(op: (context: Context, save: Saver) -> Void) -> SignalProducer<Void, NoError>
+    
+    /**
+     Executes the given operation in a background thread.
+     
+     - parameter operation: Operation to be executed. Context must be used to save your changes in and the save() closure must be called in order to get the changes persisted in the storage.
+     
+     - returns: Observable that executes the action.
+     */
+    func rx_backgroundOperation(op: (context: Context, save: Saver) -> Void) -> Observable<Void>
     
     /**
      Executes a request.
@@ -36,6 +55,15 @@ public protocol ReactiveStorage {
      - returns: SignalProducer that executes the action.
      */
     func rac_fetch<T>(request: Request<T>) -> SignalProducer<[T], Error>
+    
+    /**
+     Executes a request.
+     
+     - parameter request: Request to be executed.
+     
+     - returns: Observable that executes the action.
+     */
+    func rx_fetch<T>(request: Request<T>) -> Observable<[T]>
 
     /**
      Executes a background fetch mapping the response into a PONSO thread safe entity.
@@ -46,6 +74,16 @@ public protocol ReactiveStorage {
      - returns: SignalProducer that executes the action.
      */
     func rac_backgroundFetch<T, U>(request: Request<T>, mapper: T -> U) -> SignalProducer<[U], Error>
+    
+    /**
+     Executes a background fetch mapping the response into a PONSO thread safe entity.
+     
+     - parameter request: Request to be executed.
+     - parameter mapper:  Mapper.
+     
+     - returns: Observable that executes the action.
+     */
+    func rx_backgroundFetch<T, U>(request: Request<T>, mapper: T -> U) -> Observable<[U]>
 }
 
 
@@ -61,13 +99,30 @@ public extension ReactiveStorage where Self: Storage {
     
     - returns: SignalProducer that executes the action.
     */
-    func rac_operation(operation: (context: Context, save: Saver) -> Void) -> SignalProducer<Void, NoError> {
+    func rac_operation(op: (context: Context, save: Saver) -> Void) -> SignalProducer<Void, NoError> {
         return SignalProducer { (observer, disposable) in
             self.operation { (context, saver) in
-                operation(context: context, save: saver)
+                op(context: context, save: saver)
                 observer.sendCompleted()
             }
         }
+    }
+    
+    /**
+     Executes the given operation.
+     
+     - parameter operation: Operation to be executed. Context must be used to save your changes in and the save() closure must be called in order to get the changes persisted in the storage.
+     
+     - returns: Observable that executes the action.
+     */
+    func rx_operation(op: (context: Context, save: Saver) -> Void) -> Observable<Void> {
+        return create({ (observer) -> RxSwift.Disposable in
+            self.operation({ (context, saver) -> Void in
+                op(context: context, save: saver)
+                observer.onCompleted()
+            })
+            return NopDisposable.instance
+        })
     }
     
     /**
@@ -77,15 +132,32 @@ public extension ReactiveStorage where Self: Storage {
      
      - returns: SignalProducer that executes the action.
      */
-    func rac_backgroundOperation(operation: (context: Context, save: Saver) -> Void) -> SignalProducer<Void, NoError> {
+    func rac_backgroundOperation(op: (context: Context, save: Saver) -> Void) -> SignalProducer<Void, NoError> {
         return SignalProducer { (observer, disposable) in
             let priority = DISPATCH_QUEUE_PRIORITY_DEFAULT
             dispatch_async(dispatch_get_global_queue(priority, 0)) {
                 self.operation { (context, saver) in
-                    operation(context: context, save: saver)
+                    op(context: context, save: saver)
                     observer.sendCompleted()
                 }
             }
+        }
+    }
+    
+    /**
+     Executes the given operation in a background thread.
+     
+     - parameter operation: Operation to be executed. Context must be used to save your changes in and the save() closure must be called in order to get the changes persisted in the storage.
+     
+     - returns: Observable that executes the action.
+     */
+    func rx_backgroundOperation(op: (context: Context, save: Saver) -> Void) -> Observable<Void> {
+        return create { (observer) -> RxSwift.Disposable in
+            self.operation { (context, saver) in
+                op(context: context, save: saver)
+                observer.onCompleted()
+            }
+            return NopDisposable.instance
         }
     }
     
@@ -121,6 +193,38 @@ public extension ReactiveStorage where Self: Storage {
     }
     
     /**
+     Executes a background fetch mapping the response into a PONSO thread safe entity.
+     
+     - parameter request: Request to be executed.
+     - parameter mapper:  Mapper.
+     
+     - returns: Observable that executes the action.
+     */
+    func rx_backgroundFetch<T, U>(request: Request<T>, mapper: T -> U) -> Observable<[U]> {
+        let observable: Observable<[T]> = create({ (observer) -> RxSwift.Disposable in
+            let priority = DISPATCH_QUEUE_PRIORITY_DEFAULT
+            dispatch_async(dispatch_get_global_queue(priority, 0)) {
+                do {
+                    let results = try self.saveContext.fetch(request)
+                    observer.onNext(results)
+                    observer.onCompleted()
+                }
+                catch {
+                    if let error = error as? Error {
+                        observer.onError(error)
+                    }
+                    else {
+                        observer.onNext([])
+                        observer.onCompleted()
+                    }
+                }
+            }
+            return NopDisposable.instance
+        })
+        return observable.map({$0.map(mapper)}).observeOn(MainScheduler.sharedInstance)
+    }
+    
+    /**
      Executes a request.
      
      - parameter request: Request to be executed.
@@ -143,5 +247,31 @@ public extension ReactiveStorage where Self: Storage {
                 }
             }
         }
+    }
+    
+    /**
+     Executes a request.
+     
+     - parameter request: Request to be executed.
+     
+     - returns: Observable that executes the action.
+     */
+    func rx_fetch<T>(request: Request<T>) -> Observable<[T]> {
+        return create({ (observer) -> RxSwift.Disposable in
+            do {
+                try observer.onNext(self.fetch(request))
+                observer.onCompleted()
+            }
+            catch  {
+                if let error = error as? Error {
+                    observer.onError(error)
+                }
+                else {
+                    observer.onNext([])
+                    observer.onCompleted()
+                }
+            }
+            return NopDisposable.instance
+        })
     }
 }
