5185f4081d7dcd2ad8e473b460580fc3a326f5b5
Pedro Piñera Buendía <pepibumur@gmail.com>
2016-09-05 19:09:11 +0200

    [feature/carthage] Add Realm dependency and configuration

---
+++ CarthageProjects/watchOS/watchOS.h
@@ -0,0 +1,19 @@
+//
+//  watchOS.h
+//  watchOS
+//
+//  Created by Pedro Pinera Buendia on 05/09/16.
+//  Copyright © 2016 in.caramba.SugarRecord. All rights reserved.
+//
+
+#import <WatchKit/WatchKit.h>
+
+//! Project version number for watchOS.
+FOUNDATION_EXPORT double watchOSVersionNumber;
+
+//! Project version string for watchOS.
+FOUNDATION_EXPORT const unsigned char watchOSVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <watchOS/PublicHeader.h>
+
+
