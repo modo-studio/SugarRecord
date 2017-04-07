41500b746c6957a7c1cb0b22c2c242c209ffd5fd
Pedro Piñera Buendía <pepibumur@gmail.com>
2017-04-07 15:21:27 +0200

    [background-operation] Add backgroundOperation method to the storage

--- SugarRecord/Source/Foundation/Entities/Storage.swift
+++ SugarRecord/Source/Foundation/Entities/Storage.swift
@@ -15,6 +15,7 @@ public protocol Storage: CustomStringConvertible, Requestable {
     var memoryContext: Context! { get }
     func removeStore() throws
     func operation<T>(_ operation: @escaping (_ context: Context, _ save: @escaping () -> Void) throws -> T) throws -> T
+    func backgroundOperation(_ operation: @escaping (_ context: Context, _ save: @escaping () -> Void) -> (), completion: @escaping (Error?) -> ())
     func fetch<T: Entity>(_ request: FetchRequest<T>) throws -> [T]
     
 }
